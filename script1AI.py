dbb6c65fa35c253ac12da04dabdf6eadf5527f7e
Felix <83243533+MrScruffilius@users.noreply.github.com>
2023-03-06 13:19:49 +0100

    Update script1AI.py
    finished the project

--- script1AI.py
+++ script1AI.py
@@ -1,13 +1,9 @@
-
-import math
-
+# FIELD POSITIONS
 # 0 | 1 | 2 
 # 3 | 4 | 5
 # 6 | 7 | 8 
-#user is O=1
-# #AI is X =0
-
-
+#User is O=1
+#AI is X =0
 
 def main():
     print("Welcome to AI-Tic-Tac-Toe")
@@ -27,32 +23,15 @@ def main():
     print("\n\nGAME DONE!\n")
     printfield(field)
 
-
-
-# Ausgang der Funktion:
-# 3 unentschieden
-# 2 User hat gewonnen
-# 1 AI hat gewonnen
-# 0 kein ende
-
-
 def check_win(field_array):
-    if (field_array[0]==field_array[1]==field_array[2]==0) or (field_array[0]==field_array[1]==field_array[2]==1):
+    for i in range(3):
+        if (field_array[0+i*3]==field_array[1+i*3]==field_array[2+i*3] and field_array[0+i*3]!=0):
+            return	field_array[0+i*3]
+        if (field_array[0+i]==field_array[3+i]==field_array[6+i] and field_array[0+i]!=0):
+            return	field_array[0+i]
+    if (field_array[0]==field_array[4]==field_array[8]==1) or (field_array[0]==field_array[4]==field_array[8]==2):
         return	field_array[0]
-    elif (field_array[3]==field_array[4]==field_array[5]==0) or (field_array[3]==field_array[4]==field_array[5]==1):
-        return	field_array[3]
-    elif (field_array[6]==field_array[7]==field_array[8]==0) or (field_array[6]==field_array[7]==field_array[8]==1):
-        return	field_array[6]
-    elif (field_array[0]==field_array[3]==field_array[6]==0) or (field_array[0]==field_array[3]==field_array[6]==1):
-        return field_array[0]
-    elif (field_array[1]==field_array[4]==field_array[7]==0) or (field_array[1]==field_array[4]==field_array[7]==1):
-        return field_array[1]
-    elif (field_array[2]==field_array[5]==field_array[8]==0) or (field_array[2]==field_array[5]==field_array[8]==1):
-        return field_array[2]
-    
-    if (field_array[0]==field_array[4]==field_array[8]==0) or (field_array[0]==field_array[4]==field_array[8]==1):
-        return	field_array[0]
-    elif (field_array[2]==field_array[4]==field_array[6]==0) or (field_array[2]==field_array[4]==field_array[6]==1):
+    elif (field_array[2]==field_array[4]==field_array[6]==1) or (field_array[2]==field_array[4]==field_array[6]==2):
         return field_array[2]
     elif 0 not in field_array:
         return 3
@@ -74,47 +53,43 @@ def utils(field_array, turn):
     for i in range(9):
         if field_array[i]==0:
             possible_moves.append(i)
-    #probability for a move
-    probability=1/len(possible_moves)
-    #iterates through the tree and adds the utils
-    for j in possible_moves:
-        copy = list.copy(field_array)
-        copy[j]=turn
-        utility=utility+(utils(copy,(turn+1)%2)*probability)
-    return utility
+    if turn==0:
+        max=-9000000000
+        for j in possible_moves:
+            copy = list.copy(field_array)
+            copy[j]=1
+            utility=(utils(copy,1))
+            if utility>max:
+                max = utility
+        return utility
+    else:
+        probability=1/len(possible_moves)
+        for j in possible_moves:
+            copy = list.copy(field_array)
+            copy[j]=2
+            utility=utility+(utils(copy,(turn+1)%2)*probability)
+        return utility
 
-
-
-
-
-
-#returns the caluclated best branch
 def get_AI_answer(field_array):
     possible_moves=[]
     for i in range(9):
         if field_array[i]==0:
             possible_moves.append(i)
-    #probability for a move
     probability=1/len(possible_moves)
-    #iterates through the tree and adds the utils
     max=-9000000000
     position=0
     for j in possible_moves:
         copy = list.copy(field_array)
-        copy[j]=0
+        copy[j]=1
         utility=(utils(copy,1)*probability)
         if utility>max:
             max = utility
             position = j
     return position
 
-
-#prints the field
 def printfield(field_array):
     for i in range(3):
-        c0 = ' ' 
-        c1 = ' '
-        c2 = ' '
+        c0 = c1 = c2 = ' '
         if field_array[0+(i*3)]==1:
             c0='X'
         elif field_array[0+(i*3)]==2:
